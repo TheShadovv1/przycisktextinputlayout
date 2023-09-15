647d2ee93f06120460ae48a91e28b3be1380c873
TheShadovv1 <Piort.Czurynski201@gmail.com>
2023-09-15 09:22:13 +0200

    button + textinputlayout

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.mojprzycisk).setOnClickListener {
+            findViewById<Button>(R.id.mojprzycisk).text =
+                findViewById<TextInputEditText>(R.id.mojtextinput).text
+        }
+    }
+}
