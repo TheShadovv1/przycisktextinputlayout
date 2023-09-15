bcc3a68c5a485a1bbf295de0b279b4942e31eacb
TheShadovv1 <Piort.Czurynski201@gmail.com>
2023-09-15 09:58:37 +0200

    ekran logowania

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,63 @@
-package com.example.myapplication
+package com.example.kotlinexample
+
+
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+
+import android.text.TextUtils.isEmpty
+
 import android.widget.Button
-import com.google.android.material.textfield.TextInputEditText
+
+import android.widget.EditText
+
+import android.widget.Toast
+import com.example.myapplication.R
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.mojprzycisk).setOnClickListener {
-            findViewById<Button>(R.id.mojprzycisk).text =
-                findViewById<TextInputEditText>(R.id.mojtextinput).text
+
+
+
+        var username = findViewById<EditText>(R.id.username)
+
+        var password = findViewById<EditText>(R.id.password)
+
+        var loginbutton = findViewById<Button>(R.id.loginbutton)
+
+
+
+        // set on-click listener
+
+        loginbutton.setOnClickListener {
+
+            val username = username.text;
+
+            val password = password.text;
+
+
+
+            if(isEmpty(username) || isEmpty(password)){
+
+                Toast.makeText(this@MainActivity, "Please fill all fields", Toast.LENGTH_LONG).show()
+
+            }else{
+
+                // your code to validate the user_name and password combination
+
+                // and verify the same
+
+            }
+
         }
+
     }
+
 }
